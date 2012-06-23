618ff446869baefc25250aacbb6885f29993003a
Dave Kroondyk <davekaro@gmail.com>
2012-06-23 09:21:40 -0400

    users spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,5 +36,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find params[:id]
+    @user.destroy
+
+    redirect_to users_path, :notice => 'User deleted'
   end
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe 'users' do
+  before :each do
+    user = create(:user)
+    visit users_path
+  end
+
+  it 'displays the users' do
+    page.should have_selector '.page-header', :text => 'Users'
+    page.should have_content 'John Doe'
+  end
+
+  it 'creates a user' do
+    click_link 'New user'
+    fill_in 'First name',   :with => 'Dave'
+    fill_in 'Last name',    :with => 'Kroondyk'
+    fill_in 'Email',        :with => 'davekaro@gmail.com'
+    fill_in 'Phone number', :with => '555-555-5555'
+    click_button 'Create User'
+
+    page.should have_selector '.alert.alert-success', :text => 'User created'
+    page.should have_content 'Dave Kroondyk'
+  end
+
+  it 'shows errors for invalid users' do
+    click_link 'New user'
+    click_button 'Create User'
+
+    page.should have_selector '.alert.alert-block.alert-error'
+    page.should have_content "First name can't be blank"
+  end
+
+  it 'edits a user' do
+    click_link 'Edit'
+    fill_in 'Last name', :with => 'Smith'
+    click_button 'Update User'
+
+    page.should have_selector '.alert.alert-success', :text => 'User updated'
+    page.should have_content 'John Smith'
+    page.should_not have_content 'John Doe'
+  end
+
+  it 'deletes a user' do
+    click_link 'Delete'
+
+    page.should have_selector '.alert.alert-success', :text => 'User deleted'
+    page.should_not have_content 'John Doe'
+  end
+end
