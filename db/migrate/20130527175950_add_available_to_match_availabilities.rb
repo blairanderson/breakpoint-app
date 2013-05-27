d721550b6f3179a2b1bbc15b9a12609fe164c7c6
Dave Kroondyk <davekaro@gmail.com>
2013-05-27 19:15:37 -0400

    migrate availabilities

--- db/migrate/20130527175950_add_available_to_match_availabilities.rb
+++ db/migrate/20130527175950_add_available_to_match_availabilities.rb
@@ -1,5 +1,6 @@
 class AddAvailableToMatchAvailabilities < ActiveRecord::Migration
   def change
     add_column :match_availabilities, :available, :boolean
+    execute 'UPDATE match_availabilities SET available = true'
   end
 end
