a322f9d2437292eca275b4514938b6bd42a42a0e
Dave Kroondyk <davekaro@gmail.com>
2013-05-27 19:17:43 -0400

    fix migration rollback

--- db/migrate/20130527175950_add_available_to_match_availabilities.rb
+++ db/migrate/20130527175950_add_available_to_match_availabilities.rb
@@ -1,6 +1,11 @@
 class AddAvailableToMatchAvailabilities < ActiveRecord::Migration
-  def change
+  def up
     add_column :match_availabilities, :available, :boolean
     execute 'UPDATE match_availabilities SET available = true'
   end
+
+  def down
+    remove_column :match_availabilities, :available
+  end
 end
+
