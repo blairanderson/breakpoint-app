89fe4777a64520d01a10751ad7a38e5d7c62330d
Dave Kroondyk <davekaro@gmail.com>
2013-08-14 17:01:47 -0400

    some production configs

--- Capfile
+++ Capfile
@@ -5,7 +5,7 @@ require 'capistrano/setup'
 require 'capistrano/deploy'
 
 # Includes tasks from other gems included in your Gemfile
-require 'capistrano/rbenv'
+require 'capistrano/chruby'
 require 'capistrano/bundler'
 require 'capistrano/rails'
 
---
+++ config/deploy/unicorn/production.rb
@@ -0,0 +1,37 @@
+root = '/home/breakpointapp/app/current'
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.stderr.log"
+stdout_path "#{root}/log/unicorn.stdout.log"
+
+listen '/tmp/unicorn.breakpointapp_production.sock'
+worker_processes 2
+timeout 30
+
+preload_app true
+
+before_fork do |server, worker|
+  # Disconnect since the database connection will not carry over
+  if defined? ActiveRecord::Base
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Quit the old unicorn process
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # Start up the database connection again in the worker
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
+
