c37bdb2d94ebcadb46b94a00793f0f5a0ae230d9
Dave Kroondyk <davekaro@gmail.com>
2013-08-15 14:08:55 -0400

    remove default configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,6 @@
 set :application,   'breakpoint-app'
 set :repo_url,      'git@github.com:davekaro/breakpoint-app.git'
-set :branch,        'master'
 set :deploy_to,     '/home/breakpointapp/app'
-set :keep_releases, 5
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
