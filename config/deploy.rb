7f7ba9b8e535d0bfb4af0bf26aec19c25030049d
Dave Kroondyk <davekaro@gmail.com>
2013-08-16 15:16:25 -0400

    link some dirs too

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,5 +3,5 @@ set :repo_url,      'git@github.com:davekaro/breakpoint-app.git'
 set :deploy_to,     '/home/breakpointapp/app'
 
 set :linked_files, %w{.env config/unicorn.rb}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets}
 
