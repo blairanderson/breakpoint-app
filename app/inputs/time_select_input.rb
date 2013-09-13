016e725cde64f3cc23c32a08612f6de3ec21c651
Dave Kroondyk <davekaro@gmail.com>
2013-09-13 15:39:17 -0400

    WIP screwing with simple form

--- app/inputs/time_select_input.rb
+++ app/inputs/time_select_input.rb
@@ -1,29 +1,6 @@
-class TimeSelectInput < SimpleForm::Inputs::CollectionSelectInput
-  private
-
-  def collection
-    generate_hours
-  end
-
-  def generate_hours
-    time_options = []
-    6.upto 11 do |hour|
-      time_options << generate_minutes(hour, 'AM')
-    end
-
-    time_options << generate_minutes(12, 'PM')
-
-    1.upto 11 do |hour|
-      time_options << generate_minutes(hour, 'PM')
-    end
-
-    time_options.flatten
-  end
-
-  def generate_minutes(hour, am_or_pm)
-    [0, 30].map do |minute|
-      "%02d:%02d #{am_or_pm}" % [hour, minute]
-    end
+class TimeSelectInput < SimpleForm::Inputs::StringInput
+  def input
+    template.content_tag(:div, super, :class => "input-append bootstap-timepicker")
   end
 end
 
