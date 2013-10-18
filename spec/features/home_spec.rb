88cde344c26af20a2145109e61fa2447fa5984b2
Dave Kroondyk <davekaro@gmail.com>
2013-10-18 11:37:25 -0400

    update home page text

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'home' do
   it 'loads home page with nobody signed in' do
     visit root_path
-    page.should have_content "makes tennis team scheduling easy"
+    page.should have_content "Tennis team scheduling made easy"
   end
 end
 
