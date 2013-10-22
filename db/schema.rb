f8a740df330ada91bb3acbc94d5132318a51b871
Sudershan Bhandari <sudershan.bhandari@gmail.com>
2013-10-22 17:35:37 -0400

    Schema Change from Dave

--- db/schema.rb
+++ db/schema.rb
@@ -93,8 +93,8 @@ ActiveRecord::Schema.define(version: 20131023154918) do
     t.integer  "practice_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "team_id"
     t.boolean  "available"
+    t.integer  "team_id"
   end
 
   add_index "practice_sessions", ["practice_id"], name: "index_practice_sessions_on_practice_id", using: :btree
