c2a452bbe0961b22d1d0d869cb5b92ccfce48eca
Sudershan Bhandari <sudershan.bhandari@gmail.com>
2013-11-10 22:21:35 -0500

    Non captain shouldn't edit/delete team

--- app/policies/team_policy.rb
+++ app/policies/team_policy.rb
@@ -2,6 +2,10 @@ class TeamPolicy < ApplicationPolicy
   def update?
     captain?
   end
+
+  def destroy?
+    captain?
+  end
   
   def captain?
     member = user.team_members.where(:team_id => record.id).first
