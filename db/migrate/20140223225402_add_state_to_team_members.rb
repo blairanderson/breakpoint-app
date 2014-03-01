1f7ab737911b21fef0acb957781d041ba50a8f1a
Dave Kroondyk <davekaro@gmail.com>
2014-03-01 16:29:00 -0500

    try fixing no invite in migration

--- db/migrate/20140223225402_add_state_to_team_members.rb
+++ db/migrate/20140223225402_add_state_to_team_members.rb
@@ -1,6 +1,8 @@
 class AddStateToTeamMembers < ActiveRecord::Migration
   def up
     add_column :team_members, :state, :string, default: "new", null: false
+    class Invite < ActiveRecord::Base
+    end
     TeamMember.all.each do |tm|
       invite = Invite.where(user_id: tm.user_id, team_id: tm.team_id).first
       state = tm.read_attribute(:active) ? "active" : "inactive"
