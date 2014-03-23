b529e1c106d81469beaf6d3ebd861975c0602483
Mike Munroe <mike@mikemunroe.com>
2014-03-23 10:30:31 -0400

    refactor out create_team_with_captain_and_member

--- spec/features/team_members_spec.rb
+++ spec/features/team_members_spec.rb
@@ -3,17 +3,13 @@ require 'spec_helper'
 describe 'team members' do
   before :each do
     login_captain
-    @team = create(:team)
-    ActsAsTenant.current_tenant = @team
-    @team.team_members.create(:user => @captain, :role => 'captain', :state => 'active')
-    @user2 = create(:user2)
-    ActsAsTenant.current_tenant = nil
+    create_team_with_captain_and_member
     visit team_team_members_path(@team)
   end
 
   it 'edit team members role' do
     page.should have_content 'Captain'
-    click_link 'Edit'
+    find("a[href='#{edit_team_team_member_path(@team, @captain_member)}']").click
     select 'Member', :from => 'team_member_role'
     click_button 'Save settings'
     page.should have_content 'Team member updated'
@@ -22,9 +18,6 @@ describe 'team members' do
   end
 
   it 'team member deactivates self' do
-    ActsAsTenant.current_tenant = @team
-    @team.team_members.create(:user => @user2, :role => 'member', :state => 'active')
-    ActsAsTenant.current_tenant = nil
     click_link 'Sign out'
     login @user2
     visit team_team_members_path(@team)
@@ -34,13 +27,19 @@ describe 'team members' do
   end
 
   it 'captain can deactive team member' do
-    ActsAsTenant.current_tenant = @team
-    @member = @team.team_members.create(:user => @user2, :role => 'member', :state => 'active')
-    ActsAsTenant.current_tenant = nil
     visit team_team_members_path(@team)
     find("a[href='#{edit_team_team_member_path(@team, @member)}']").click
     click_button 'Deactivate team membership'
     page.should have_selector '.alert.alert-success', :text => 'Team member is now inactive'
   end
+end
+
+def create_team_with_captain_and_member
+  @team = create(:team)
+  ActsAsTenant.current_tenant = @team
+  @captain_member = @team.team_members.create(:user => @captain, :role => 'captain', :state => 'active')
+  @user2 = create(:user2)
+  @member = @team.team_members.create(:user => @user2, :role => 'member', :state => 'active')
+  ActsAsTenant.current_tenant = nil
 end
 
