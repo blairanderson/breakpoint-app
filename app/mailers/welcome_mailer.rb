6ec2340647d9c8ae31636509b79eead0f78a327d
Dave Kroondyk <davekaro@gmail.com>
2014-05-25 20:45:25 -0400

    add link to change password in welcome email

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -4,20 +4,19 @@ class WelcomeMailer < ActionMailer::Base
   layout 'mailer'
 
   def new_user_welcome(options)
-    @team_member = TeamMember.find(options.fetch(:team_member_id))
-    @token = @team_member.user.reset_password_token!
     welcome_email(options)
   end
 
   def welcome(options)
-    @team_member = TeamMember.find(options.fetch(:team_member_id))
     welcome_email(options)
   end
 
   def welcome_email(options)
-    @team_name     = @team_member.team.name
-    @mail_type     = "Welcome"
-    @from          = options.fetch(:from)
+    @team_member = TeamMember.find(options.fetch(:team_member_id))
+    @team_name   = @team_member.team.name
+    @mail_type   = "Welcome"
+    @token       = @team_member.user.reset_password_token!
+    @from        = options.fetch(:from)
     mail :to    => @team_member.user.email,
       :from     => formatted_from(options.fetch(:from)),
       :reply_to => options.fetch(:reply_to),
