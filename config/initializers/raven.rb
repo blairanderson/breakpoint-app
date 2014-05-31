778be19e2b6920eb239417d6dc0853b243e50826
Dave Kroondyk <davekaro@gmail.com>
2014-05-31 14:23:28 -0400

    let's try not ignoring any errors and see what we get

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -4,5 +4,7 @@ require 'raven/sidekiq'
 Raven.configure do |config|
   config.dsn = "https://#{Rails.application.secrets.sentry_public_key}:#{Rails.application.secrets.sentry_secret_key}@app.getsentry.com/#{Rails.application.secrets.sentry_project_id}"
   config.environments = %w[ production ]
+  # override defaults in https://github.com/getsentry/raven-ruby/blob/master/lib/raven/configuration.rb#L79
+  config.excluded_exceptions = []
 end
 
